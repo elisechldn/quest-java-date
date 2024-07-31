57c7f03651c2663bc46c826776c419837a3f3b37
elisechldn <elise.chaldjian@gmail.com>
2024-08-01 00:35:56 +0200

    Handling and discovering date management APIs

--- TestDate.java
+++ TestDate.java
@@ -2,7 +2,8 @@ import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 import java.util.Date;
-import java.util.Scanner; 
+import java.util.Scanner;
+import java.text.ParseException;
 
 class TestDate {
 
@@ -18,6 +19,29 @@ class TestDate {
         sc.close();
 
         // TODO : your code after this line
+        //Using a GregorianCalendar class in order to instanciate a date chosen by the User
+        GregorianCalendar calendar = new GregorianCalendar(year, month - 1, day);
         
+        //We retrieve the day chosen by the user in order to know the exact day of that particular week
+        int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK);
+
+        //Date has been formatted using the class SDF in order to only get the informations we need (not the time)
+        SimpleDateFormat format = new SimpleDateFormat("MM-dd-yyyy");
+
+        //We are applying the format on the data contained in calendar
+        String dateString = format.format(calendar.getTime());
+
+        //try&catch is mandatory while using SDF
+        try {
+            
+            /*dateString = str under MM-dd-yyyyparse = Method from Date. Convert Str to Obj
+            format.parse() = method from Date used to analyze the Str. If the Str is correctly written
+            it becomes an Obj held in concatDate */
+            Date concatDate = format.parse(dateString);
+            System.out.println("The date is : " + format.format(concatDate)); 
+            System.out.println("The day of week is : " + dayOfWeek);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
     }
 }
